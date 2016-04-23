449e67f0b2a758ea1e7074add9700242cf4866d5
vaidaabelkyte <vaidilutia@gmail.com>
2016-04-23 14:28:27 +0100

    finished

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,42 @@
 angular.module('starter.controllers', [])
 
-.controller('tasksCtrl', function($scope, Tasks) {
-    $scope.tasks = Tasks.all;
+.controller('tasksCtrl', function($scope) {
+    var newTask = {
+        newTaskName: "",
+        newTaskText: ""
+    };
     
-})
-
-.controller('addTaskCtrl', function($scope, Tasks) {
+  var tasks = [{
+    id: 0,
+    taskName: 'Go to Shop',
+    taskText: 'Water, lemon'
+  }, {
+    id: 1,
+    taskName: 'Clean Room',
+    taskText: 'Clean windows'
+  }, {
+    id: 2,
+    taskName: 'Buy clothes',
+    taskText: 'River Island and Brown Thomas'
+  }, {
+    id: 3,
+    taskName: 'Study',
+    taskText: '5-8pm'
+  }];
+    
+    function addTask(){
+        tasks.push({id: tasks.length+1, taskName: newTask.newTaskName, taskText: newTask.newTaskText});
+        newTask.newTaskName = "";
+        newTask.newTaskText = "";
+    }
+    
+    function deleteTask(task){
+        tasks.splice(tasks.indexOf(task),1);
+    }
+    
+    $scope.tasks = tasks;
+    $scope.newTask = newTask;
+    $scope.addTask = addTask;
+    $scope.deleteTask = deleteTask;
 })
 ;
--- www/js/services.js
+++ www/js/services.js
@@ -3,24 +3,8 @@ angular.module('starter.services', [])
 .factory('Tasks', function() {
   // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var tasks = [{
-    id: 0,
-    taskName: 'Go to shop',
-    taskText: 'Walk'
-  }, {
-    id: 1,
-    taskName: 'Clean Room',
-    taskText: 'Clean windows'
-  }, {
-    id: 2,
-    taskName: 'Buy clothes',
-    taskText: 'River Island and Brown Thomas'
-  }, {
-    id: 3,
-    taskName: 'Study',
-    taskText: '5-8pm'
-  }];
+  
+ 
 
   return {
     all: tasks,
